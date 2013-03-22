e70ec56d8cbb86a7926c2c39b2d104afa5f888ab
Luka Mirosevic <luka@goonbee.com>
2013-03-22 15:58:43 +0100

    added nodefly

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+require('nodefly').profile(
+    '7ac5417c-874e-49c6-81cf-ea13c72a1300',
+    [APPLICATION_NAME,'Heroku'],
+    options // optional
+);
+
 require('coffee-script')
 
 var cloudBox = require('./app/cloud-box.coffee');
